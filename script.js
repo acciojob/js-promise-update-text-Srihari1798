3937a393928b8b7c0106fc540f85eae6539912a9
Srihari1798 <120098197+Srihari1798@users.noreply.github.com>
2023-03-20 00:03:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const delayMessage = () => {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve('Hello, world!');
+          }, 1000);
+        });
+      };
+      const updateText = async () => {
+        const message = await delayMessage();
+        document.getElementById('output').innerHTML = message;
+      };
+      updateText();
